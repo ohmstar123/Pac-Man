aa8214f00fa021b70b67e93151e0691c8f7cb292
ohmstar123 <ohmpate114@ed.amdsb.ca>
2018-09-17 13:57:10 -0400

    Add project files.

---
+++ Pac-Man/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Pac_Man
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Run_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Run_Button
+            // 
+            this.Run_Button.Location = new System.Drawing.Point(317, 358);
+            this.Run_Button.Name = "Run_Button";
+            this.Run_Button.Size = new System.Drawing.Size(155, 60);
+            this.Run_Button.TabIndex = 0;
+            this.Run_Button.Text = "Run";
+            this.Run_Button.UseVisualStyleBackColor = true;
+            this.Run_Button.Click += new System.EventHandler(this.Run_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Run_Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Run_Button;
+    }
+}
+
---
+++ Pac-Man/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Pac_Man
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        
+        {
+
+        }
+        
+        private void Run_Button_Click(object sender, EventArgs e)
+        {
+            Run_Button.Visible = false;
+
+            Graphics g = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Red, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            ///                 x   y   x    y
+            g.DrawLine(drawPen, 0, 135, 250, 135);
+            g.DrawLine(drawPen, 0, 45, 350, 45);
+
+            /// change of direction in tunnel
+            g.DrawLine(drawPen, 250, 130, 250, 300);
+            g.DrawLine(drawPen, 350, 40, 350, 300);
+
+            /// pac-man colors
+            Pen PacPen = new Pen(Color.Red, 10);
+            SolidBrush PacBrush = new SolidBrush(Color.Yellow);
+
+            /// pac-man 
+            g.FillPie(PacBrush, 0, 60, 60, 60, 55, 260);
+            Thread.Sleep(500);
+            
+            
+
+
+
+
+
+
+
+        }
+    }
+}
